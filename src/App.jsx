da3b7079ccc66f53356d757c4846196d9582af2f
julces180 <jul972009@gmail.com>
2024-08-23 07:02:45 -0500

    Color corregido listo para empezar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { MapComponent } from "./MapComponent";
+import { Navbar, Nav } from "react-bootstrap"; // Importa Navbar y Nav
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'; // Asegúrate de importar el CSS global
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Idat GPS</h1>
-      <MapComponent />
+    <div className="app-container">
+      <div className="sidebar bg-dark text-white">
+        <Navbar bg="bg-dark" variant="dark" className="flex-column">
+          <Navbar.Brand href="#home">Idat GPS</Navbar.Brand>
+          <Nav className="flex-column">
+            <Nav.Link href="#/action-1">Sedes</Nav.Link>
+            <Nav.Link href="#/action-2">Lugares de interés cercanos</Nav.Link>
+          </Nav>
+        </Navbar>
+      </div>
+      <div className="content">
+        <MapComponent />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/MapComponent.jsx
+++ src/MapComponent.jsx
@@ -1,38 +1,36 @@
-// src/MapComponent.jsx
-
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import './MapComponent.module.css'; // Asegúrate de importar el CSS global
 
-// Solución para íconos que no se muestran correctamente en Leaflet + Webpack/Vite
+// Solución para íconos que no se muestran correctamente en Leaflet
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
   iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
   shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
 });
 
 export const MapComponent = () => {
-  const position = [51.505, -0.09]; // Coordenadas iniciales (Londres)
+  const position = [-12.157033, -76.980799]; // Coordenadas iniciales
 
   return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      style={{ height: "500px", width: "100%" }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>
-          Bienvenido a Idat GPS! <br /> Aquí estamos.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className="map-wrapper">
+      <MapContainer
+        center={position}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }} // Asegura que el mapa ocupe todo el contenedor
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>Bienvenido a Idat GPS! Aquí estamos.</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 };
